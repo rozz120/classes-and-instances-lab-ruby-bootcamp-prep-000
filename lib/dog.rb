dcf8e0642aa4e93519e1c6b5903b4fedf45fad0e
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-18 10:39:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,9 +6,10 @@ end
 fido = Dog.new
   snoopy = Dog.new
   lassie = Dog.new
-  adele_goldberg = Person.new
-  alan_kay  = Person.new
+  
 
 class Person
   
 end
+adele_goldberg = Person.new
+  alan_kay  = Person.new
